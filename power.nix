ade112bf6423712d5bd9e65572636fd24d514e11
Floweynt <50057682+Floweynt@users.noreply.github.com>
2025-12-09 15:40:06 -0600

    forgot to check some things in

---
+++ power.nix
@@ -0,0 +1,26 @@
+{
+    pkgs,
+    lib,
+    ...
+}:
+{
+    powerManagement = {
+        enable = true;
+    };
+    services = {
+        upower.enable = true;
+        auto-cpufreq = {
+            enable = true;
+            settings = {
+                battery = {
+                    governor = "powersave";
+                    turbo = "never";
+                };
+                charger = {
+                    governor = "performance";
+                    turbo = "auto";
+                };
+            };
+        };
+    };
+}
---
+++ programs/keepassxc.nix
@@ -0,0 +1,13 @@
+{
+    clangPkgs,
+    user,
+    ...
+}:
+{
+    home-manager.users."${user}".programs.keepassxc = {
+        enable = true;
+        package = clangPkgs.keepassxc;
+    };
+}
+
+
